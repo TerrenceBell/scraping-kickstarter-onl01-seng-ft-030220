b5c274c333a74f9a9442add0a768e4979cd083e3
Terrence Bell <terrencekbell@gmail.com>
2020-03-22 22:11:53 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,5 +12,5 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("ul.project-meta span.location-name").text
+
 create_project_hash
-project.css("span.location-name")<span class="location-name">Brooklyn, NY</span>
